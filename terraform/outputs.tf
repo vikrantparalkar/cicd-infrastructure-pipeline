676298c9930ade7ca7a0b67303b508bd91b1cf34
Vikrant Paralkar <114619358+vikrantparalkar@users.noreply.github.com>
2025-11-27 01:12:38 +0530

    new
    Added kubeconfig output for EKS cluster connection.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,18 +1,28 @@
 output "cluster_name" {
-  description = "EKS cluster name"
   value       = module.eks.cluster_id
 }
 
 output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
   value       = module.eks.cluster_endpoint
 }
 
 output "cluster_certificate_authority_data" {
-  description = "CA data for the cluster"
   value       = module.eks.cluster_certificate_authority_data
 }
 
 output "node_group_names" {
   value = keys(module.eks.eks_managed_node_groups)
 }
+
+#gives full kubeconfig YAML for the EKS cluster to connect
+output "kubeconfig" {
+  value       = module.eks.kubeconfig
+}
+
+
+# another way to connect with the aws cluster
+
+# output "eks_update_kubeconfig_command" {
+#   description = "AWS CLI command to update kubeconfig for the EKS cluster"
+#   value       = "aws eks update-kubeconfig --region ${var.region} --name ${module.eks.cluster_id}"
+# }
